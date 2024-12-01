228353a0c28244fd2dc69f5376881a8ca4c83009
pyae-phyo-paing <paing.mdy.edu@gmail.com>
2024-12-01 20:50:48 +0630

    paingblog modefied dec1,2024

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/',[App\Http\Controllers\FrontController::class, 'blogHome'])->name(
 
 Route::get('/blog-post/{id}',[App\Http\Controllers\FrontController::class, 'blogPost'])->name('blog-post');
 
+Route::group(['prefix'=>'backend','as'=>'backend.'], function(){
+    Route::get('/',[App\Http\Controllers\Admin\AdminController::class, 'index'])->name('admin-dashboard');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
