56d8b3f9ffabd3d7962c264eb9adf67a131f0c52
pyae-phyo-paing <paing.mdy.edu@gmail.com>
2024-12-05 15:15:42 +0630

    paingBlog project dec5, 2024

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+    use SoftDeletes;
+    protected $table = 'posts';
+    protected $fillable = [
+        'title',
+        'image',
+        'description',
+        'category_id',
+        'user_id'
+    ];
 }
